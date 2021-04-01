3abbc64141f9732239753da895a1567ef7b1e3e3
catanswer <564029918@qq.com>
2021-04-01 15:39:12 +0800

    init: project files

---
+++ example/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>{{ txt }}</div>
+</template>
+<script setup lang="ts">
+  import { ref } from 'vue'
+  
+  const txt = ref<string>('我是一段测试的小代码！')
+  console.log('Test Word')
+
+</script>
