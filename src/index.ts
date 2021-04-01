2c4c640b027571a35aba86411c868bba5f7f9174
catanswer <564029918@qq.com>
2021-04-01 15:40:40 +0800

    add: plugin core files

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import type { Plugin } from 'vite'
+import { createFilter } from 'rollup-pluginutils'
+import { Options } from './types'
+
+// merge field function
+function mergeField(field: RegExp | Array<RegExp> | undefined, defaultValue: Array<RegExp> ): Array<RegExp> {
+  if (field) {
+    if (Array.isArray(field) && field.length > 0) {
+      return defaultValue.concat(field)
+    } else {
+      return [field as RegExp, ...defaultValue]
+    }
+  } else {
+    return defaultValue
+  }
+}
+
+export default function(options: Options = {
+  patterns: []
+}): Plugin {
+
+  // merge exclude field
+  const includeDefault: Array<RegExp> = [/src/]
+  let includes = mergeField(options.include, includeDefault)
+
+  // merge exclude field
+  const excludeDefault: Array<RegExp> = [/node_modules/]
+  let excludes = mergeField(options.exclude, excludeDefault)
+
+  const filter = createFilter(includes, excludes)
+
+  return {
+    name: 'vite-plugin-clear-sth',
+    apply: 'build',
+    transform(code, id) {
+      if (!filter(id)) return
+
+      let generatedCode = ''
+      options.patterns.forEach(item => {
+        generatedCode = code.replace(item, '')
+      })
+      return {
+        code: generatedCode
+      }
+    }
+  }
+}
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export interface Options {
+  patterns: RegExp[]
+  include?: RegExp[] | RegExp,
+  exclude?: RegExp[] | RegExp
+}
